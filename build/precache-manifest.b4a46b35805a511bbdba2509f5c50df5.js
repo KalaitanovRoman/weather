0a4d4bd0c3b896b5041e10b44c5d55a3f340fcc7
kalaitanov <kalaitanov@reksoft.ru>
2020-02-17 10:07:43 +0300

    Fix and build project

---
+++ build/precache-manifest.b4a46b35805a511bbdba2509f5c50df5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d157c32ca4553f1d7446c94d2ed916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3ce9d8f865fbdebce04",
+    "url": "/static/css/main.8cf64429.chunk.css"
+  },
+  {
+    "revision": "e3b60a3b12d964f73a5d",
+    "url": "/static/js/2.53d77ac3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.53d77ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ce9d8f865fbdebce04",
+    "url": "/static/js/main.ee17a738.chunk.js"
+  },
+  {
+    "revision": "ce9f8ec3a9654a7590b1",
+    "url": "/static/js/runtime-main.23ac1c3a.js"
+  },
+  {
+    "revision": "a3c4fa487c9625dfe1d337e980400e6f",
+    "url": "/static/media/desktop-background.a3c4fa48.jpg"
+  },
+  {
+    "revision": "f098141da44e10b4b49a742240cae6d1",
+    "url": "/static/media/dinosaur.f098141d.png"
+  },
+  {
+    "revision": "7d5c735ee9e9c5332ed362329a1d0c3b",
+    "url": "/static/media/mobile-background.7d5c735e.jpg"
+  }
+]);
